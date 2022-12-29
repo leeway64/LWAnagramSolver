5bb032d2d97a30df72d78d8af2bb54850cedee81
leeway64 <lee.way56@comcast.net>
2022-12-29 11:00:47 -0800

    Work on get-dictionary.sh
    - Use command -v and find in the Bash script

--- include/get-dictionary.sh
+++ include/get-dictionary.sh
@@ -1,6 +1,19 @@
-if [ "$#" -ne 2 ]; then
-    echo "Provide only the name of the text file as an argument."
+if [ "$#" -ne 1 ]; then
+    echo "Error: Provide the name of the text file as an argument."
     exit
 fi
 
-wget https://courses.cs.washington.edu/courses/cse143/22su/homework/a6/$1 -P include
+
+# Check if bun has been downloaded. Discard the output of command -v bun into /dev/null
+# Refer to this for more info: https://stackoverflow.com/a/677212
+DIR=`command -v bun`
+if command -v bun > /dev/null
+then
+    # Download the data from the UW CSE 143 Autumn 2022 website
+    wget https://courses.cs.washington.edu/courses/cse143/22su/homework/a6/$1 -P include/
+fi
+
+# Search for a file with find
+DIR=$(find ./include -name download-success.message.txt)
+echo -n "$1"
+cat $DIR
