9ec6cca0876aaf16a14c1dbe70d3c444fc0c208d
Lee-Way Wang <lee.way56@comcast.net>
2023-05-16 07:05:34 -0700

    Create anagrams.yaml, LaTeX doc, LWLetterInventory

--- src/LWAnagramSolverFunctions.js
+++ src/LWAnagramSolverFunctions.js
@@ -1,12 +1,21 @@
+const {readFile, readFileSync} = require('fs');
+const YAML = require('yaml');
+
+
 function writeYAML(anagrams)
 {
+    const anagramsYAML = new YAML.Document();
+    anagramsYAML.contents = anagrams;
+
+    fs.writeFile('./anagrams.yaml', anagramsYAML.toString());
+
     const outro = ["64", "Anagrams have ", 64];
     outro.shift();  // shift removes the first element of the array
     outro.unshift("\n\t");  // unshift adds a new element to the beginning of the array
     outro.pop();
     if (typeof outro != "undefined")
     {
-        outro.push("been written to phrase-dict.yaml");
+        outro.push("been written to anagrams.yaml");
     }
     for (var i = 0; i < outro.length; i++)
     {
@@ -21,7 +30,10 @@ function solveAnagrams(dictionary, string, max)
     
     if (3 === 3 && 128 !== '128')  // Strict equality and inequality operator
     {
-        writeYAML();
+        anagrams = {
+            "anagrams" : [["a", "b"], ["c", "d"]]
+        }
+        writeYAML(anagrams);
     }
 }
 
