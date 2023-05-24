d1c182fa6f513f2579002b1bd1fd9d11666f1915
leeway64 <lee.way56@comcast.net>
2023-05-23 20:33:45 -0700

    Work on functions in ArraysStringsHashMaps.js
    - Write test cases

--- test/ArraysStringsHashMaps.js
+++ test/ArraysStringsHashMaps.js
@@ -3,31 +3,72 @@
 // Checks if string has all unique letters
 function isUniqueV1(str)
 {
+    var lowercase = str.toLowerCase();
     var letterCounts = new Map();
-    for (let i = 0; i < str.length; i++)
+    for (let i = 0; i < lowercase.length; i++)
     {
-        if (!letterCounts.has(str[i]))
+        if (!letterCounts.has(lowercase[i]))
         {
-            letterCounts[str[i]] = 0;            
+            letterCounts.set(lowercase[i], 0);            
         }
-        letterCounts[str[i]]++;
+        letterCounts.set(lowercase[i], letterCounts.get(lowercase[i]) + 1);
     }
     
-    letterCounts.forEach ((value, key) =>
+    for (let [key, value] of letterCounts)
     {
-        if (key > 1)
+        if (value > 1)
         {
             return false;
         }
-    })
+    }
     
     return true;
 }
 
 function isPermutationV1(str1, str2)
 {
+    if (str1.length != str2.length)
+    {
+        return false;
+    }
+ 
+    var lowercase1 = str1.toLowerCase();
+    var lowercase2 = str2.toLowerCase();
+    var lowercase1Map = new Map();
+    var lowercase2Map = new Map();   
+    for (let i = 0; i < lowercase1.length; i++)
+    {
+        if (!lowercase1Map.has(lowercase1[i]))
+        {
+            lowercase1Map.set(lowercase1[i], 0);            
+        }
+        lowercase1Map.set(lowercase1[i], lowercase1Map.get(lowercase1[i]) + 1);
+    }
+    
+    
+    for (let i = 0; i < lowercase2.length; i++)
+    {
+        if (!lowercase2Map.has(lowercase2[i]))
+        {
+            lowercase2Map.set(lowercase2[i], 0);            
+        }
+        lowercase2Map.set(lowercase2[i], lowercase2Map.get(lowercase2[i]) + 1);
+    }
+    
+    for (let [key, value] of lowercase1Map)
+    {
+        if (lowercase2Map.get(key) != lowercase1Map.get(key))
+        {
+            return false;
+        }
+    }
+    
     return true;
 }
 
+function URLify(str)
+{
 
-module.exports = { isUniqueV1, isPermutationV1 };
+}
+
+module.exports = { isUniqueV1, isPermutationV1, URLify };
--- test/test-suite.js
+++ test/test-suite.js
@@ -12,6 +12,8 @@ describe('ArraysStringsHashMaps', function()
             assert.equal(ArraysStringsHashMaps.isUniqueV1("Y"), true);
             assert.equal(ArraysStringsHashMaps.isUniqueV1("ab"), true);
             assert.equal(ArraysStringsHashMaps.isUniqueV1("abba"), false);
+            assert.equal(ArraysStringsHashMaps.isUniqueV1("zzzzzz"), false);
+            assert.equal(ArraysStringsHashMaps.isUniqueV1("aaAABBcc"), false);  
         });
     });
     
@@ -19,7 +21,27 @@ describe('ArraysStringsHashMaps', function()
     {
         it('isPermutation', function()
         {
-            assert.equal(ArraysStringsHashMaps.isPermutationV1(""), true);
+            assert.equal(ArraysStringsHashMaps.isPermutationV1("", ""), true);
+            assert.equal(ArraysStringsHashMaps.isPermutationV1("a", "A"), true);
+            assert.equal(ArraysStringsHashMaps.isPermutationV1("bab", "abb"), true);
+            assert.equal(ArraysStringsHashMaps.isPermutationV1("add", "dad"), true);
+            assert.equal(ArraysStringsHashMaps.isPermutationV1("free", "tibet"), false);
+            assert.equal(ArraysStringsHashMaps.isPermutationV1("Hong", "Kong"), false);
+            assert.equal(ArraysStringsHashMaps.isPermutationV1("", "1989"), false);
+        });
+    });
+    
+    describe('URLify', function()
+    {
+        it('URLify', function()
+        {
+            assert.equal(ArraysStringsHashMaps.URLify(""), "");
+            assert.equal(ArraysStringsHashMaps.URLify("Tibet"), "");
+            assert.equal(ArraysStringsHashMaps.URLify("East Turkistan"), "");
+            assert.equal(ArraysStringsHashMaps.URLify("Free Tibet"), "");
+            assert.equal(ArraysStringsHashMaps.URLify("Glory to Hong Kong"), "");
+            assert.equal(ArraysStringsHashMaps.URLify("The Chinese Communist Party is the most vile political party in the world"), "");
+            assert.equal(ArraysStringsHashMaps.URLify("I support Taiwanese independence"), "");
         });
     });
 });
