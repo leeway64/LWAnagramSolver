12164db32231899c0b09b75c0be2ae5238ea2cf5
leeway64 <lee.way56@comcast.net>
2023-05-25 22:14:59 -0700

    Work on functions in ArraysStringsHashMaps.js
    - Write test cases

--- test/ArraysStringsHashMaps.js
+++ test/ArraysStringsHashMaps.js
@@ -25,6 +25,7 @@ function isUniqueV1(str)
     return true;
 }
 
+// Checks if str1 and str2 are permutations of each other
 function isPermutationV1(str1, str2)
 {
     if (str1.length != str2.length)
@@ -66,9 +67,32 @@ function isPermutationV1(str1, str2)
     return true;
 }
 
+// Replaces every space between words with "%20"
 function URLify(str)
 {
-
+    var trimmed_str = str.trim();
+    return trimmed_str.replaceAll(" ", "%20");
 }
 
-module.exports = { isUniqueV1, isPermutationV1, URLify };
+// Checks if s2 is a substring of s1
+function isSubstring(s1, s2)
+{
+    var lowercase1 = s1.toLowerCase();
+    var lowercase2 = s2.toLowerCase();
+    return lowercase1.includes(lowercase2);
+}
+
+// Checks if str2 is a rotation of s1
+function isRotation(str1, str2)
+{
+    var lowercase1 = str1.toLowerCase();
+    var lowercase2 = str2.toLowerCase();
+    return isSubstring(lowercase2.concat(lowercase2), lowercase1);
+}
+
+// 
+function zeroRowColumn()
+{
+}
+
+module.exports = { isUniqueV1, isPermutationV1, URLify, isSubstring, isRotation, zeroRowColumn };
--- test/test-suite.js
+++ test/test-suite.js
@@ -36,12 +36,33 @@ describe('ArraysStringsHashMaps', function()
         it('URLify', function()
         {
             assert.equal(ArraysStringsHashMaps.URLify(""), "");
-            assert.equal(ArraysStringsHashMaps.URLify("Tibet"), "");
-            assert.equal(ArraysStringsHashMaps.URLify("East Turkistan"), "");
-            assert.equal(ArraysStringsHashMaps.URLify("Free Tibet"), "");
-            assert.equal(ArraysStringsHashMaps.URLify("Glory to Hong Kong"), "");
-            assert.equal(ArraysStringsHashMaps.URLify("The Chinese Communist Party is the most vile political party in the world"), "");
-            assert.equal(ArraysStringsHashMaps.URLify("I support Taiwanese independence"), "");
+            assert.equal(ArraysStringsHashMaps.URLify("Tibet"), "Tibet");
+            assert.equal(ArraysStringsHashMaps.URLify("East Turkistan"), "East%20Turkistan");
+            assert.equal(ArraysStringsHashMaps.URLify("   Free Tibet     "), "Free%20Tibet");
+            assert.equal(ArraysStringsHashMaps.URLify("Glory to Hong Kong"), "Glory%20to%20Hong%20Kong");
+            assert.equal(ArraysStringsHashMaps.URLify("The Chinese Communist Party is the most vile political party in the world"), "The%20Chinese%20Communist%20Party%20is%20the%20most%20vile%20political%20party%20in%20the%20world");
+            assert.equal(ArraysStringsHashMaps.URLify("I support Taiwanese independence"), "I%20support%20Taiwanese%20independence");
+        });
+    });
+    
+    describe('isSubstring', function()
+    {
+        it('isSubstring', function()
+        {
+            assert.equal(ArraysStringsHashMaps.isSubstring("Free Tibet", "Tibet"), true);
+            assert.equal(ArraysStringsHashMaps.isSubstring("Hong Kong", " "), true);
+            assert.equal(ArraysStringsHashMaps.isSubstring("A Song of Ice and Fire", "A SONG OF"), true);
+            assert.equal(ArraysStringsHashMaps.isSubstring("The Winds", "of Winter"), false);
+        });
+    });
+    
+    describe('isRotation', function()
+    {
+        it('isRotation', function()
+        {
+            assert.equal(ArraysStringsHashMaps.isRotation("waterbottle", "erbottlewat"), true);
+            assert.equal(ArraysStringsHashMaps.isRotation("Thrones", "NESTHRO"), true);
+            assert.equal(ArraysStringsHashMaps.isRotation("Hong Kong", "fhdskfhsjkf"), false);
         });
     });
 });
